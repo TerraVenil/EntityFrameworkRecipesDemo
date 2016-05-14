be1cae04a28c456792883860a0864b3e5cc29f8e
Oleksiy Borodenko <oboroden@softserveinc.com>
2016-05-14 23:03:30 +0300

    Add GraphDiff to support update entity graph.

---
+++ Demo3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RefactorThis.GraphDiff;
+
+namespace Demo3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var dbContext = GetDbContext())
+            {
+                var customerId = "ANATR";
+                var customer = dbContext.Customers
+                    .Include(x => x.Orders.Select(d => d.Order_Details))
+                    .AsNoTracking().
+                    Single(x => x.CustomerID == customerId);
+
+                customer.ContactTitle = "Owner 1";
+                
+                var order = customer.Orders.First();
+                customer.Orders.Remove(order);
+
+                dbContext.UpdateGraph(customer, map => map.OwnedCollection(c => c.Orders, o => o.OwnedCollection(d => d.Order_Details)));
+
+                dbContext.SaveChanges();
+            }
+        }
+
+        private static NorthwindEntities GetDbContext()
+        {
+            var dbContext = new NorthwindEntities();
+
+            dbContext.Configuration.AutoDetectChangesEnabled = false;
+            dbContext.Configuration.LazyLoadingEnabled = false;
+            dbContext.Configuration.ProxyCreationEnabled = false;
+
+            return dbContext;
+        }
+    }
+}

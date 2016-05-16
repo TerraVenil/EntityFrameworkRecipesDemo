4b97c80f8da4d1aa8e8cfcfdded2dd9d04dfc297
Oleksiy Borodenko <oboroden@softserveinc.com>
2016-05-16 21:50:00 +0300

    Add partial updates / valdiation samples.

---
+++ Demo5/NorthwindEntitiesExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace Demo5
+{
+    public static class NorthwindEntitiesExtensions
+    {
+        public static void ExcludeProductPropertiesFromUpdate(this NorthwindEntities dbContext, Product product)
+        {
+            dbContext.Entry(product).Property(p => p.ProductName).IsModified = false;
+            dbContext.Entry(product).Property(p => p.SupplierID).IsModified = false;
+            dbContext.Entry(product).Property(p => p.QuantityPerUnit).IsModified = false;
+            dbContext.Entry(product).Property(p => p.UnitPrice).IsModified = false;
+            dbContext.Entry(product).Property(p => p.UnitsInStock).IsModified = false;
+            dbContext.Entry(product).Property(p => p.UnitsOnOrder).IsModified = false;
+            dbContext.Entry(product).Property(p => p.ReorderLevel).IsModified = false;
+            dbContext.Entry(product).Property(p => p.Discontinued).IsModified = false;
+        }
+    }
+}
---
+++ Demo5/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Data.Entity;
+using System.Diagnostics;
+
+namespace Demo5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var dbContext = GetDbContext())
+            {
+                var categoryId = 7;
+                var product = new Product
+                              {
+                                  ProductID = 1,
+                                  ProductName = "Chai",
+                                  SupplierID = 1,
+                                  CategoryID = categoryId, // 1
+                                  QuantityPerUnit = "10 boxes x 20 bags",
+                                  UnitPrice = 18.00M,
+                                  UnitsInStock = 39,
+                                  UnitsOnOrder = 0,
+                                  ReorderLevel = 10,
+                                  Discontinued = false
+                              };
+
+                dbContext.Set<Product>().Attach(product);
+                dbContext.Entry(product).State = EntityState.Modified;
+
+                dbContext.ExcludeProductPropertiesFromUpdate(product);
+
+                //dbContext.Configuration.ValidateOnSaveEnabled = false;
+
+                dbContext.SaveChanges();
+            }
+        }
+
+        private static NorthwindEntities GetDbContext()
+        {
+            var dbContext = new NorthwindEntities();
+
+            dbContext.Database.Log = s => Debug.WriteLine(s);
+
+            dbContext.Configuration.LazyLoadingEnabled = false;
+            dbContext.Configuration.ProxyCreationEnabled = false;
+
+            return dbContext;
+        }
+    }
+}
